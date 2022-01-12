37c10a79136c0b4d4f19cef03031ffc88ff52802
Zolival <zolinak@gmail.com>
2022-01-12 22:39:11 +0100

    I couldn't resolve to add the btn btn-primary class to the pricing-page.component.ts file**

--- src/app/pricing-page/pricing-page.component.ts
+++ src/app/pricing-page/pricing-page.component.ts
@@ -17,7 +17,7 @@ export class PricingPageComponent implements OnInit {
   priceTable: IPriceItem[] = [
     {name: "Free", price: 0, options:["10 tanuló", "2 Gb tárhely", "Email támogatás", "Dokumentáció" ], btnText: "Ingyenes regisztráció"},
     {name: "Pro", price: 15, options:["20 tanuló", "10 Gb tárhely", "Kiemelt email támogatás", "Dokumentáció" ], btnText: "Kezdő lépések"},
-    {name: "Enterprice", price: 29, options:["100 tanuló", "100 Gb tárhely", "Telefopnos és email támogatás", "Dokumentáció" ], btnText: "Kontact"}
+    {name: "Enterprice", price: 29, options:["100 tanuló", "100 Gb tárhely", "Telefopnos és email támogatás", "Dokumentáció" ], btnText: "Kontakt"}
   ];
 
   constructor() { }
